60ab30b878124223210794557e1741fa2a63264b
TabrezHira <51405264+Tabrezhira@users.noreply.github.com>
2025-02-16 05:10:29 +0530

    Day-1 (16-feb)
     NavBar

---
+++ src/sections/index.js
@@ -0,0 +1,20 @@
+import Hero from './Hero'
+import Footer from './Footer'
+import Offers from './Offers'
+import Products from './Products'
+import Quality from './Quality'
+import Reviews from './Reviews'
+import Services from './Services'
+import Subscribe from './Subscribe'
+
+
+export{
+    Hero,
+    Footer,
+    Offers,
+    Products,
+    Quality,
+    Reviews,
+    Services,
+    Subscribe
+}
