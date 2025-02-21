cee2748763e6946b1c7cc611526a0313f7fe8f45
TabrezHira <51405264+Tabrezhira@users.noreply.github.com>
2025-02-22 04:09:03 +0530

    done(22-feb)

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,12 +1,14 @@
-
-
-function Button({label, iconURL}) {
+function Button({ label, iconURL, backgroundColor, borderColor, textColor, fullWidth }) {
   return (
-    <button className="flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg bg-coral-red rounded-full text-white border-coral-red leading-none">
-        {label}
-        {iconURL && <img src={iconURL} alt="arrow right icon" className="ml-2 rounded-full w-5 h-5"/>}
+    <button
+      className={`flex justify-center items-center gap-2 px-7 py-4 border rounded-full font-montserrat text-lg leading-none 
+      ${backgroundColor ? `${backgroundColor} ${textColor} ${borderColor}` : `bg-coral-red text-white border-coral-red`} 
+      ${fullWidth ? 'w-full' : ''}`}
+    >
+      {label}
+      {iconURL && <img src={iconURL} alt="icon" className="ml-2 rounded-full w-5 h-5" />}
     </button>
-  )
+  );
 }
 
-export default Button
+export default Button;
